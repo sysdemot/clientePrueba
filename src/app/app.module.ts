c6b06934ce76cea8aade7b8e7de8ec57904efb36
Dennis Ortega <sysdemot@gmail.com>
2020-12-12 12:12:39 -0500

    Login de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,20 @@ import { IconsModule } from './icons/icons.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormLoginComponent } from './auth/components/form-login/form-login.component';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { ConfigurationComponent } from './config/components/configuration/configuration.component';
 import { DashMainComponent } from './dashboard/components/dash-main/dash-main.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { AdminComponent } from './shared/components/admin/admin.component';
-import { SidebarMenuComponent } from './shared/components/sidebar-menu/sidebar-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormLoginComponent,
     LoginComponent,
     ConfigurationComponent,
     DashMainComponent,
     AdminComponent,
-    NavBarComponent,
-    SidebarMenuComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
